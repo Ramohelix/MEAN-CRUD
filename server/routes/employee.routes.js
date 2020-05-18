2dedde81a9756b09374c856775a1049a6feb94ac
Ramohelix <angl_dx@hotmail.com>
2020-05-17 21:18:52 -0700

    Mean-employees-v1

---
+++ server/routes/employee.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const emploCtrl = require('../controllers/employee.controller');
+
+router.get('/', emploCtrl.getEmployees);
+router.post('/', emploCtrl.createEmployees);
+router.get('/:id', emploCtrl.getEmployee);
+router.put('/:id', emploCtrl.editEmployee);
+router.delete('/:id', emploCtrl.deleteEmployee);
+
+module.exports = router;
